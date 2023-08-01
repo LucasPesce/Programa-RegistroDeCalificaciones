8ae13fe9538cbbc6a0e5acb8cc9aaa4911114be3
Lucas Pesce <117691263+LucasPesce@users.noreply.github.com>
2023-08-01 11:06:52 -0300

    Add files via upload

---
+++ Registro-Calificaciones/Ejercicio 2/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Ejercicio_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.txtCalificacion = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtMenorCalificacion = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMayorCalificacion = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPromedio = new System.Windows.Forms.TextBox();
+            this.txtCantCalificaciones = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnRegistrar);
+            this.groupBox1.Controls.Add(this.txtCalificacion);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(409, 86);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Registro de Calificaciones";
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(286, 24);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(96, 37);
+            this.btnRegistrar.TabIndex = 2;
+            this.btnRegistrar.Text = "&Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // txtCalificacion
+            // 
+            this.txtCalificacion.Location = new System.Drawing.Point(103, 33);
+            this.txtCalificacion.Name = "txtCalificacion";
+            this.txtCalificacion.Size = new System.Drawing.Size(100, 20);
+            this.txtCalificacion.TabIndex = 1;
+            this.txtCalificacion.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCalificacion_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calificacion";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtMenorCalificacion);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtMayorCalificacion);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txtPromedio);
+            this.groupBox2.Controls.Add(this.txtCantCalificaciones);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.btnConsultar);
+            this.groupBox2.Location = new System.Drawing.Point(12, 119);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(409, 223);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Consultas";
+            // 
+            // txtMenorCalificacion
+            // 
+            this.txtMenorCalificacion.Location = new System.Drawing.Point(187, 124);
+            this.txtMenorCalificacion.Name = "txtMenorCalificacion";
+            this.txtMenorCalificacion.ReadOnly = true;
+            this.txtMenorCalificacion.Size = new System.Drawing.Size(70, 20);
+            this.txtMenorCalificacion.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Menor Calificacion";
+            // 
+            // txtMayorCalificacion
+            // 
+            this.txtMayorCalificacion.Location = new System.Drawing.Point(187, 94);
+            this.txtMayorCalificacion.Name = "txtMayorCalificacion";
+            this.txtMayorCalificacion.ReadOnly = true;
+            this.txtMayorCalificacion.Size = new System.Drawing.Size(70, 20);
+            this.txtMayorCalificacion.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Mayor Calificacion";
+            // 
+            // txtPromedio
+            // 
+            this.txtPromedio.Location = new System.Drawing.Point(187, 62);
+            this.txtPromedio.Name = "txtPromedio";
+            this.txtPromedio.ReadOnly = true;
+            this.txtPromedio.Size = new System.Drawing.Size(70, 20);
+            this.txtPromedio.TabIndex = 5;
+            // 
+            // txtCantCalificaciones
+            // 
+            this.txtCantCalificaciones.Location = new System.Drawing.Point(187, 28);
+            this.txtCantCalificaciones.Name = "txtCantCalificaciones";
+            this.txtCantCalificaciones.ReadOnly = true;
+            this.txtCantCalificaciones.Size = new System.Drawing.Size(70, 20);
+            this.txtCantCalificaciones.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Calificacion Promedio";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Cantidad de calificaciones";
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(286, 19);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(96, 37);
+            this.btnConsultar.TabIndex = 3;
+            this.btnConsultar.Text = "&Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 378);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.TextBox txtCalificacion;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtMenorCalificacion;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMayorCalificacion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPromedio;
+        private System.Windows.Forms.TextBox txtCantCalificaciones;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConsultar;
+    }
+}
+
---
+++ Registro-Calificaciones/Ejercicio 2/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_2
+{
+    public partial class Form1 : Form
+    {
+        const int MAX = 10;                 // Maximo Elementos de la LISTA
+        public int [] calificaciones;       // DECLARACION de la LISTA
+        public int contador;                // Contador de elementos de la LISTA
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            calificaciones = new int [MAX];             // CREACION de la LISTA
+            contador = 0;                               // INICIALIZACION del contador
+            txtCalificacion.Text = "";
+            txtCantCalificaciones.ReadOnly = true;
+            txtMayorCalificacion.ReadOnly = true;
+            txtMenorCalificacion.ReadOnly= true;
+            txtPromedio.ReadOnly = true;
+
+            for(int i=0; i<calificaciones.Length; i++)
+            {
+                calificaciones[i] = 0;
+            }
+        }
+        private void txtCalificacion_KeyPress(object sender, KeyPressEventArgs e)       //Evento PRESIONAR TECLA
+        {
+            if (!(Char.IsDigit(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))       // Si no es digito ni "Back"
+            {
+                e.Handled = true;                                                   // No permitir el tipeo
+            }
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(txtCalificacion.Text) >= 1 && int.Parse(txtCalificacion.Text) <= 10)
+            {
+                calificaciones[contador] = int.Parse(txtCalificacion.Text);         // Cumplida la condicion, se guarda la nota en la LISTA
+                contador++;                                                         // AUMENTA el contador.
+                txtCalificacion.Text = "";
+                txtCalificacion.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un digito entre 1 al 10", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtCalificacion.Text = "";
+            }
+
+            if(contador == MAX)
+            {
+                MessageBox.Show("Se completo la capacidad de la carga de datos", "ATENCION",MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                btnRegistrar.Enabled = false;
+            }
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            int SUMA = 0;
+            int calificacionMayor = calificaciones[0];
+            int calificacionMenor = calificaciones[0];
+
+
+            for (int i= 0; i < contador; i++)
+            {
+                SUMA += calificaciones[i];
+
+                    //OBTENER LAS NOTA MAYOR Y MENOR
+                    if (calificaciones[i] > calificacionMayor)
+                    {
+                        calificacionMayor = calificaciones[i];
+                    }
+
+                    if (calificaciones[i] < calificacionMenor)
+                    {
+                        calificacionMenor = calificaciones[i];
+                    }
+            }
+            float PROMEDIO = (float)SUMA / contador;
+
+            txtCantCalificaciones.Text = contador.ToString();           // Indica la cantidad de notas registradas            
+            txtPromedio.Text = PROMEDIO.ToString("F2");                 // Indica el promedio de las notas
+            txtMayorCalificacion.Text = calificacionMayor.ToString();   // Indica la mayor nota
+            txtMenorCalificacion.Text = calificacionMenor.ToString();   // Indica la menor nota
+        }
+    }
+}
